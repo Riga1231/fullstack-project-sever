a2b147b3d72401e04fb5d4dc75a400dfeeb93600
Riga1231 <riga_jericho@plpasig.edu.ph>
2025-07-20 07:08:57 +0800

    a

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = 5000;
+const db = require("./db");
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Backend API is online");
+app.get("/", async (req, res) => {
+  try {
+    const [rows] = await db.query("SELECT NOW() AS current_time");
+    res.send(`Connected! Server time is: ${rows[0].current_time}`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Failed to connect to DB: " + error.message);
+  }
 });
 
 app.get("/api/hello", (req, res) => {
