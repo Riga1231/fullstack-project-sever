94250d52e818f2e8a78bb4127624524a79594a46
Riga1231 <riga_jericho@plpasig.edu.ph>
2025-07-20 05:25:43 +0800

    Added a default route

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend API is online");
+});
+
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from backend!" });
 });
