4565702135df3eaa4ae4f05401e4e3cb75b9f069
Riga1231 <riga_jericho@plpasig.edu.ph>
2025-07-20 17:15:31 +0800

    a

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2/promise");
 
 let pool;
 
-if (process.env.DATABASE_URL) {
+if (process.env.MYSQL_URL) {
   // Production (e.g., Railway)
   const { URL } = require("url");
-  const dbUrl = new URL(process.env.DATABASE_URL);
+  const dbUrl = new URL(process.env.MYSQL_URL);
 
   pool = mysql.createPool({
     host: dbUrl.hostname,
@@ -14,16 +14,5 @@ if (process.env.DATABASE_URL) {
     password: dbUrl.password,
     database: dbUrl.pathname.slice(1), // removes the leading slash
   });
-} else {
-  // Development (local)
-  require("dotenv").config(); // only needed for local
-  pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "", // your local password
-    database: "plp_db",
-    port: 3306,
-  });
 }
-
 module.exports = pool;
