283f4747c07d8ee6b4689ecf8be89b3e7dd04e93
Riga1231 <riga_jericho@plpasig.edu.ph>
2025-07-20 16:53:15 +0800

    a

--- db.js
+++ db.js
@@ -1,7 +1,14 @@
 const mysql = require("mysql2/promise");
 
+const dbUrl = new URL(process.env.MYSQL_URL);
+
+// Extract and convert to mysql2 format
 const pool = mysql.createPool({
-  uri: process.env.MYSQL_URL,
+  host: dbUrl.hostname,
+  user: dbUrl.username,
+  password: dbUrl.password,
+  database: dbUrl.pathname.replace("/", ""), // remove leading slash
+  port: dbUrl.port,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
